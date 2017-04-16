fcb31f6f53076654e5c2a57936ef1c5176d1752d
Rushanthan <rushan26@live.com>
2017-04-16 19:18:31 +0530

    Sidebar widgets

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
+<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+  <?php if(is_active_sidebar('sidebar')) : ?>
+    <?php dynamic_sidebar('sidebar'); ?>
+<?php endif; ?>
+</div><!-- /.row -->
+
+</div><!-- /.container -->
+
 <footer class="blog-footer">
   <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
   <p>
--- functions.php
+++ functions.php
@@ -21,4 +21,20 @@
 
   add_filter('excerpt_length', 'set_excerpt_length');
 
+  // Widget location
+  function wpb_init_widgets($id) {
+    register_sidebar();
+  }
+
+  register_sidebar(array(
+    'name' => 'Sidebar',
+    'id' => 'sidebar',
+    'before_widget' => '<div class="sidebar-module">',
+    'after_widget' => '</div>',
+    'before_title' => '<h4>',
+    'after_title' => '</h4>'
+  ));
+
+  add_action('widgets_init', 'wpb_init_widgets');
+
  ?>
